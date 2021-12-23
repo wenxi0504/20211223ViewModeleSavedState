0ce6e4752dbbe6038d60e035db7eba373795bccb
Sophie <wenxi1988@hotmail.com>
2021-12-23 01:25:02 -0500

    version2:Using SavedStateHandle

--- app/src/main/java/com/example/a20211223viewmodelesavedstate/MainActivity.java
+++ app/src/main/java/com/example/a20211223viewmodelesavedstate/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a20211223viewmodelesavedstate;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.SavedStateViewModelFactory;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -14,18 +15,21 @@ import com.example.a20211223viewmodelesavedstate.databinding.ActivityMainBinding
 public class MainActivity extends AppCompatActivity {
     MyViewModel myViewModel;
     ActivityMainBinding binding;
-    final static String KEY_NUMBER="my_number";
+    public final static String KEY_NUMBER="my_number";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
-        myViewModel= new ViewModelProvider(this).get(MyViewModel.class);
+        myViewModel= new ViewModelProvider(this,new SavedStateViewModelFactory(getApplication(),this)).get(MyViewModel.class);
+        /*
         if(savedInstanceState !=null)
         {
             myViewModel.getNumber().setValue(savedInstanceState.getInt(KEY_NUMBER));
 
         }
+
+         */
         binding.setData(myViewModel);
         binding.setLifecycleOwner(this);
 
--- app/src/main/java/com/example/a20211223viewmodelesavedstate/MyViewModel.java
+++ app/src/main/java/com/example/a20211223viewmodelesavedstate/MyViewModel.java
@@ -1,21 +1,41 @@
 package com.example.a20211223viewmodelesavedstate;
 
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.ViewModel;
 
 public class MyViewModel extends ViewModel {
-    private MutableLiveData<Integer> number;
+   // private MutableLiveData<Integer> number;
+    private SavedStateHandle handle;
+    public MyViewModel(SavedStateHandle handle)
+    {
+        this.handle=handle;
+
+
+    }
 
     public MutableLiveData<Integer> getNumber() {
+        if (!handle.contains(MainActivity.KEY_NUMBER))
+        {
+            handle.set(MainActivity.KEY_NUMBER,0);
+
+
+        }
+
+        /*
         if (number == null)
         {
             number=new MutableLiveData<>();
             number.setValue(0);
         }
         return number;
+
+         */
+        return handle.getLiveData(MainActivity.KEY_NUMBER);
     }
 
     public void add(){
-        number.setValue(number.getValue()+1);
+       // number.setValue(number.getValue()+1);
+        getNumber().setValue(getNumber().getValue()+1);
     }
 }
